0acaba10d40a9f23fd949c6c4584b5744c086d98
vendroid <vendroid@mail.ru>
2020-06-12 18:35:43 +0300

    hw-2 complete

--- project/askme/urls.py
+++ project/askme/urls.py
@@ -19,12 +19,12 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('index/',  views.index,    name = 'index'),
-    path('login/',  views.login,    name = 'login'),
-    path('signup/', views.signup,   name = 'signup'),
-    path('ask/',    views.ask,      name = 'ask'),
-    path('hot/',    views.hot,      name = 'hot'),
+  path('index/',  views.index,    name = 'index'),
+  path('login/',  views.login,    name = 'login'),
+  path('signup/', views.signup,   name = 'signup'),
+  path('ask/',    views.ask,      name = 'ask'),
+  path('hot/',    views.hot,      name = 'hot'),
 
-    path('question/<int:qid>', views.question, name = 'question'),
-    path('tag/<str:tag>',      views.tag,      name = 'tag'),
+  path('question/<int:qid>', views.question, name = 'question'),
+  path('tag/<str:tag>',      views.tag,      name = 'tag'),
 ]
