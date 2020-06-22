6a9c1073210a8b3e63bfbbf844626a55f0a85403
vendroid <vendroid@mail.ru>
2020-06-22 18:59:47 +0300

    added gunicorn HelloWorld

---
+++ project/test.py
@@ -0,0 +1,9 @@
+
+
+def app(environ, start_response):
+  status = '200 OK'
+  output = 'Hello World!\n'
+  response_headers = [('Content-type', 'text/plain'),
+      ('Content-Length', str(len(output)))]
+  start_response(status, response_headers)
+  return [bytes(output, 'utf-8')]
